9c89996b5d2ff2ba1cd66c453db628da1b5aed88
Edwin Orlando Castro Orjuela <edwinorjuela412@gmail.com>
2026-08-12 23:25:42 -0500

    Se agrega el servicio de guardar categoria

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -3,9 +3,13 @@ package com.company.inventory.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.company.inventory.model.Category;
 import com.company.inventory.response.CategoryResponseRest;
 import com.company.inventory.services.ICategoryService;
 
@@ -16,10 +20,35 @@ public class CategoryRestController {
 	@Autowired 
 	private ICategoryService service;
 	
+	/**
+	 * get all category
+	 * @return
+	 */
     @GetMapping("/categories")
+    
 	public ResponseEntity<CategoryResponseRest> searchCategorires(){
-    	
     	ResponseEntity<CategoryResponseRest> response = service.search();
+    	return response;
+    	
+    	/**
+    	 * get category by id
+    	 * @param id
+    	 */
+}
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<CategoryResponseRest> searchCategoriresById(@PathVariable Long id){
+    	ResponseEntity<CategoryResponseRest> response = service.searchById(id);
+    	return response;
+}
+    /**
+     * save of category
+     * @param category
+     * @return
+     */
+    
+    @PostMapping("/categories")
+    public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category){
+    	ResponseEntity<CategoryResponseRest> response = service.save(category);
     	return response;
 }
 
